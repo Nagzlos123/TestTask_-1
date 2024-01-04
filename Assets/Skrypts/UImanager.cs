a634b70ba1453ef2c8c1ad89abc022a2dfb481ce
Nagzlos123 <arekwie1998@onet.pl>
2024-01-04 17:23:06 +0100

    Creating set-up of the scene. Adapting Canvas for multipul screen resolutions and formating itself accordingly

---
+++ Assets/Skrypts/UImanager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    public List<GameObject> players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ButtonCliked( GameObject playerCharakter)
+    {
+
+    }
+   public void QuitGame()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
